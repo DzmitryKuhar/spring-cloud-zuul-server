e370f73f7c8cb8e8dcc40a5b3f3ff4fa9cb9c50f
Dima <dmitriykuhar@gmail.com>
2017-08-10 11:21:46 +0300

    Initial project setup.

---
+++ src/main/java/com/iucalyptus/spring/cloud/zuul/server/ZuulServerApplication.java
@@ -0,0 +1,22 @@
+package com.iucalyptus.spring.cloud.zuul.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <p>
+ * Date: 08/10/2017
+ *
+ * @author Dzmitry Kuhar
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+}
